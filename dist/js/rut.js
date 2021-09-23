921223be4eb5f7f281d44a89ae34b04147d34f2e
JonaOlave <jonathan.olave@outlook.com>
2021-09-23 11:52:37 -0300

    Se rediseña formulario contacto con bootstrap

--- dist/js/rut.js
+++ dist/js/rut.js
@@ -1,3 +1,5 @@
+console.log("fuera del boton");
+// Formato RUT
 $("#rut")
   .rut({formatOn: 'keyup', validateOn: 'keyup'})
   .on('rutInvalido', function(){ 
@@ -6,3 +8,41 @@ $("#rut")
   .on('rutValido', function(){ 
     $(this).parents(".control-group").removeClass("error")
   });
+
+// Validar RUT
+
+var Fn = {
+  // Valida el rut con su cadena completa "XXXXXXXX-X"
+  validaRut : function (rutCompleto) {
+    rutCompleto = rutCompleto.replace("‐","-");
+    if (!/^[0-9]+[-|‐]{1}[0-9kK]{1}$/.test( rutCompleto ))
+      return false;
+    var tmp   = rutCompleto.split('-');
+    var digv  = tmp[1]; 
+    var rut   = tmp[0];
+    if ( digv == 'K' ) digv = 'k' ;
+    
+    return (Fn.dv(rut) == digv );
+  },
+  dv : function(T){
+    var M=0,S=1;
+    for(;T;T=Math.floor(T/10))
+      S=(S+T%10*(9-M++%6))%11;
+    return S?S-1:'k';
+  }
+}
+
+$("#btn-enviar").click(function(){
+  let rut_formateado = document.getElementById("rut").value;
+  rut_formateado = rut_formateado.replace(/\./g, '');
+  
+  console.log(rut_formateado);
+
+  console.log("dentro del boton");
+
+  if (Fn.validaRut( $(rut_formateado).val() )){
+    console.log("El rut ingresado es válido :D");
+  } else {
+    console.log("El Rut no es válido :'( ");
+  }
+});
